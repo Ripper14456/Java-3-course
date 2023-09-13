f016a524d5053d728e3ecff1bc3cd5240451419a
Ripper14456 <reaper144@ukr.net>
2023-09-13 20:51:42 +0300

    lab 2

--- src/main/java/org/example/lab2/Demo.java
+++ src/main/java/org/example/lab2/Demo.java
@@ -4,16 +4,17 @@ import org.example.lab2.entities.*;
 import org.example.lab2.serialize.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Demo {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Person serg = new Person("Serg","Mazurok",19,new Residence("Ukraine","Chernivtsi","Golovna 165"));
-        Person bogdan = new Person("Bogdan","Mazurok",16,new Residence("Ukraine","Kyiv","Pushkina 123"));
+        Person oleh = new Person("Oleh","Alekseienko",19,new Residence("Ukraine","Chernivtsi","Shevchenka 15"));
+        Person vika = new Person("Vika","Alekseienko",16,new Residence("Ukraine","Kyiv","Pushkina 123"));
 
         List<Person> myList = new ArrayList<>();
-        myList.add(serg);
-        myList.add(bogdan);
+        myList.add(oleh);
+        myList.add(vika);
 
         SerializeToJSON serializerJSON = new SerializeToJSON();
         SerializeToXml serializerToXml = new SerializeToXml();
@@ -29,7 +30,7 @@ public class Demo {
         System.out.println(serializerToXml.readFromFile("test1234.xml"));
 
         System.out.println("TXT:");
-        serializerToTxt.writeToFile(myList,"test1235.txt");
+        serializerToTxt.writeToFile(Collections.singletonList(myList),"test1235.txt");
         System.out.println(serializerToTxt.readFromFile("test1235.txt"));
     }
 }
