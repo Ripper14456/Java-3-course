f24feef7fb4d6a6785d772e8815b0163147b4ed8
Ripper14456 <reaper144@ukr.net>
2023-09-13 19:58:30 +0300

    1

--- src/main/java/org/example/Lab1.java
+++ src/main/java/org/example/Lab1.java
@@ -1,36 +1,36 @@
 package org.example;
 
-public class Variant1 {
+public class Lab1 {
     public int inMeter(int l) {
         return l / 100;
     }
-    public boolean isPositiveNumber(int number) {
-        return number > 0;
+    public boolean isPositiveNumber(int n) {
+        return n > 0;
     }
 
-    public int modifyNumber(int number) {
-        if (number > 0) {
-            number += 1;
+    public int modifyNumber(int n) {
+        if (n > 0) {
+            n += 1;
         }
-        return number;
+        return n;
     }
 
-    public String getDayOfWeek(int dayNumber) {
-        return switch (dayNumber) {
-            case 1 -> "понеділок";
-            case 2 -> "вівторок";
-            case 3 -> "середа";
-            case 4 -> "четвер";
-            case 5 -> "п'ятниця";
-            case 6 -> "субота";
-            case 7 -> "неділя";
-            default -> "невідомий день";
+    public String getDayOfWeek(int day) {
+        return switch (day) {
+            case 1 -> "monday";
+            case 2 -> "tuesday";
+            case 3 -> "wednesday";
+            case 4 -> "thursday";
+            case 5 -> "friday";
+            case 6 -> "saturday";
+            case 7 -> "sunday";
+            default -> "undefined";
         };
     }
 
     public void printNumberNTimes(int K, int N) {
         if (N <= 0) {
-            System.out.print("N має бути більше нуля");
+            System.out.print("N > 0");
             return;
         }
 
@@ -41,19 +41,18 @@ public class Variant1 {
 
     public int findRemainingLength(int A, int B) {
         if (A <= 0 || B <= 0) {
-            throw new IllegalArgumentException("Як A, так і B повинні бути позитивними числами.");
+            throw new IllegalArgumentException("A and B are positive.");
         }
 
         while (A >= B) {
             A -= B;
         }
-
         return A;
     }
 
     public int[] generateOddNumbers(int N) {
         if (N <= 0) {
-            throw new IllegalArgumentException("N повинно бути більше 0");
+            throw new IllegalArgumentException("N > 0");
         }
 
         int[] oddNumbers = new int[N];
@@ -63,7 +62,6 @@ public class Variant1 {
             oddNumbers[i] = currentOdd;
             currentOdd += 2;
         }
-
         return oddNumbers;
     }
 
@@ -79,15 +77,15 @@ public class Variant1 {
     }
 
 
-    public int[] findMinMax(int[] numbers) {
-        if (numbers == null || numbers.length == 0) {
+    public int[] findMinMax(int[] n) {
+        if (n == null || n.length == 0) {
             return new int[]{};
         }
 
-        int min = numbers[0];
-        int max = numbers[0];
+        int min = n[0];
+        int max = n[0];
 
-        for (int number : numbers) {
+        for (int number : n) {
             if (number < min) {
                 min = number;
             }
@@ -95,7 +93,6 @@ public class Variant1 {
                 max = number;
             }
         }
-
         return new int[]{min, max};
     }
 }
--- src/test/java/Lab1Test.java
+++ src/test/java/Lab1Test.java
@@ -1,4 +1,4 @@
-import org.example.Variant1;
+import org.example.Lab1;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -9,12 +9,12 @@ import java.io.PrintStream;
 import static org.testng.Assert.assertEquals;
 
 
-public class Variant1Test {
+public class Lab1Test {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Test(dataProvider = "meterData")
     public void meterTest(int L, int expected) {
-        int actual = new Variant1().inMeter(L);
+        int actual = new Lab1().inMeter(L);
         assertEquals(actual, expected);
     }
 
@@ -22,65 +22,65 @@ public class Variant1Test {
     public Object[][] meterData() {
         return new Object[][]{
                 {100, 1},
-                {350, 3},
+                {200, 2},
                 {1000, 10},
-                {1550, 15},
-                {100000, 1000}
+                {2000, 20},
+                {200000, 2000}
         };
     }
 
     @Test(dataProvider = "positiveNumberData")
-    public void testIsPositiveNumber(int number, boolean expected) {
-        boolean actual = new Variant1().isPositiveNumber(number);
+    public void testIsPositiveNumber(int n, boolean expected) {
+        boolean actual = new Lab1().isPositiveNumber(n);
         assertEquals(actual, expected);
     }
 
     @DataProvider
     public Object[][] positiveNumberData() {
         return new Object[][]{
-                {5, true},
+                {1, true},
                 {0, false},
-                {-5, false},
-                {100, true},
-                {-100, false}
+                {-7, false},
+                {234, true},
+                {-224, false}
         };
     }
 
     @Test(dataProvider = "modifyNumberData")
     public void testModifyNumber(int input, int expected) {
-        int actual = new Variant1().modifyNumber(input);
+        int actual = new Lab1().modifyNumber(input);
         assertEquals(actual, expected);
     }
 
     @DataProvider
     public Object[][] modifyNumberData() {
         return new Object[][]{
-                {5, 6},
-                {-5, -5},
+                {2, 3},
+                {-25, -25},
                 {0, 0},
-                {100, 101},
-                {-100, -100}
+                {150, 151},
+                {-400, -400}
         };
     }
 
     @Test(dataProvider = "dayOfWeekData")
-    public void testGetDayOfWeek(int dayNumber, String expected) {
-        String actual = new Variant1().getDayOfWeek(dayNumber);
+    public void testGetDayOfWeek(int day, String expected) {
+        String actual = new Lab1().getDayOfWeek(day);
         assertEquals(actual, expected);
     }
 
     @DataProvider
     public Object[][] dayOfWeekData() {
         return new Object[][]{
-                {1, "понеділок"},
-                {2, "вівторок"},
-                {3, "середа"},
-                {4, "четвер"},
-                {5, "п'ятниця"},
-                {6, "субота"},
-                {7, "неділя"},
-                {0, "невідомий день"},
-                {8, "невідомий день"}
+                {1, "monday"},
+                {2, "tuesday"},
+                {3, "wednesday"},
+                {4, "thursday"},
+                {5, "friday"},
+                {6, "saturday"},
+                {7, "sunday"},
+                {0, "undefined"},
+                {8, "undefined"}
         };
     }
 
@@ -92,23 +92,23 @@ public class Variant1Test {
 
     @Test(dataProvider = "numberData")
     public void testPrintNumberNTimes(int K, int N, String expectedOutput) {
-        new Variant1().printNumberNTimes(K, N);
+        new Lab1().printNumberNTimes(K, N);
         assertEquals(expectedOutput, outContent.toString());
     }
 
     @DataProvider
     public Object[][] numberData() {
         return new Object[][]{
-                {5, 3, "555"},
-                {7, 5, "77777"},
-                {10, 1, "10"},
-                {8, -2, "N має бути більше нуля"},
+                {2, 3, "222"},
+                {5, 5, "55555"},
+                {6, 1, "6"},
+                {18, -32, "N > 0"},
         };
     }
 
     @Test(dataProvider = "remainingLengthData")
     public void testFindRemainingLength(int A, int B, int expected) {
-        int actual = new Variant1().findRemainingLength(A, B);
+        int actual = new Lab1().findRemainingLength(A, B);
         assertEquals(actual, expected);
     }
 
@@ -127,23 +127,23 @@ public class Variant1Test {
 
     @Test(dataProvider = "oddNumbersData")
     public void testGenerateOddNumbers(int N, int[] expected) {
-        int[] actual = new Variant1().generateOddNumbers(N);
+        int[] actual = new Lab1().generateOddNumbers(N);
         assertEquals(actual, expected);
     }
 
     @DataProvider
     public Object[][] oddNumbersData() {
         return new Object[][]{
-                {7, new int[]{1, 3, 5, 7, 9, 11, 13}},
-                {5, new int[]{1, 3, 5, 7, 9}},
+                {9, new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17}},
                 {3, new int[]{1, 3, 5}},
+                {5, new int[]{1, 3, 5, 7, 9}},
                 {1, new int[]{1}},
         };
     }
 
     @Test(dataProvider = "matrixData")
     public void testCreateMatrix(int M, int N, int[][] expectedMatrix) {
-        int[][] actualMatrix = new Variant1().createMatrix(M, N);
+        int[][] actualMatrix = new Lab1().createMatrix(M, N);
 
         assertEquals(expectedMatrix.length, actualMatrix.length);
         for (int i = 0; i < expectedMatrix.length; i++) {
@@ -167,8 +167,8 @@ public class Variant1Test {
     }
 
     @Test(dataProvider = "minMaxData")
-    public void testFindMinMax(int[] numbers, int[] expected) {
-        int[] actual = new Variant1().findMinMax(numbers);
+    public void testFindMinMax(int[] n, int[] expected) {
+        int[] actual = new Lab1().findMinMax(n);
         assertEquals(actual, expected);
     }
 
